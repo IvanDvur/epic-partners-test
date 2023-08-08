cfac8485106c31aac2e7f927e4fd9c8262ecddd1
ivand <ivandvurechenskiy26@gmail.com>
2023-08-08 15:02:54 +0300

    init commit

---
+++ src/main/java/com/epicpartners/countertest/CounterTestApplication.java
@@ -0,0 +1,13 @@
+package com.epicpartners.countertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicpartners/countertest/CounterTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicpartners.countertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
